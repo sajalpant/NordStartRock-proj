534ae3acf2afc8ecf101ad87de2467a9651cd14f
sajalpant <sajalpant1@gmail.com>
2023-06-22 09:57:37 -0400

    project done

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
-# NordStartRock-proj
+<h1><a href="https://dapper-pony-7db47d.netlify.app/">Clone Of NordstromRack.com</a></h1>
+
+<div>An E-Commerce Big Screen Application for Getting Clothing, Shoes, jeweleries and More daily Life Usable Things.</div>
+
+
+Live Link ==> https://nordstormrack.netlify.app/
+
+
+<h1>Tect Stack</h1>
+
+<ul>
+<li>HTML</li>
+<li>CSS</li>
+<li>JavaScript</li>
+<li>Dom-Manipulation</li>
+<li>Bootstrap</li>
+<li>Git</li>
+<li>Netlify</li>
+
+</ul>
+
+<h1>Features</h1>
+
+<ul>
+<li>Login/Sign-up</li>
+<li>Products For Men, Women && Kids</li>
+<li>Cart Functionality</li>
+</ul>
+
+<h1>About this project</h1>
+
+<p>This project is the clone of website NordstormRack.com which is a clothing and accessories selling website . This is a collaborative project consisting of 6 team members together which we clone the website with its functionalities and features. We have basically worked on the front-end using the tech stacks like HTML , JavaScript, Css </p>
+
+<h1> Some Good Screenshots Of Our Application</h1>
+
+
+
+![Screenshot (286)](https://user-images.githubusercontent.com/101568121/192554823-e280e446-62f9-437f-b48e-c92a035ddcab.png)
+
+
+## ...
+
+
+![Screenshot (287)](https://user-images.githubusercontent.com/101568121/192554966-aa45dd47-a8f7-494d-8030-fe734923560c.png)
+
+
+## ...
+
+![Screenshot (290)](https://user-images.githubusercontent.com/101568121/192555778-1c4ed195-dfb5-441f-99e7-d3656f981298.png)
+
+
+## ...
+![Screenshot (288)](https://user-images.githubusercontent.com/101568121/192554998-f52977b3-01c0-466c-82c1-360e56c0d54e.png)
+
+
---
+++ components/beauty.js
@@ -0,0 +1,102 @@
+function beauty(){
+return `
+<div id="menscloths">
+    <ul type="none"><a href="../Beauty_page/getinspired.html"> Beauty: Get Inspired</a>
+  <li>Mother's Day Gifts</li>
+  <li>New Arrivals</li>
+   <li> Best Sellers</li>
+   <li> Prestige Beauty</li>
+   <li>Natural Beauty</li>
+   <li>Beauty Under $25</li>
+  <li> Hair Care Under $25</li>
+  <li>Skin Care Under $20</li>
+  <li>Makeup Under $20</li>
+  <li>Beauty Gifts & Sets</li>
+  <li>Travel & Mini Sizes</li>
+  <li>Clearance</li>
+ </ul>
+  </div>
+
+  <div id="menscloths">
+  <ul type="none"><a href="../Beauty_page/Allmakeup.html">Shop By Brand</a>
+  <li>Charlotte Tilbury</li>
+  <li>Clinique</li>
+  <li>Drybar</li>
+  <li>L'Occitane</li>
+  <li>M.A.C. Cosmetics</li>
+  <li>Mario Badescu</li>
+  <li>Nudestix</li>
+  <li>Shiseido</li>
+  <li>Too Faced</li>
+  <li>Urban Decay</li>
+  </ul>
+  </div>
+
+  <div id="menscloths">
+  <ul type="none"><a href="../Beauty_page/Allmakeup.html">Makeup</a>
+  <li>Blush, Bronzer & Highlighter</li>
+  <li>Brushes & Tools</li>
+  <li>Concealer & Foundation</li>
+  <li>Eye Makeup</li>
+  <li>Eyebrows</li>
+  <li>Eyeshadow & Eyeliner</li>
+  <li>Gifts & Sets</li>
+  <li>Lipstick, Lip Gloss & Liner</li>
+  <li>Mascara & Lashes</li>
+  <li>Palettes</li>
+  <li>Nail Polish & Care</li>
+  <li>Powder & Setting Sprays</li>
+  <li>Primer</li>
+  </ul>
+  </div>
+
+  <div id="menscloths">
+  <ul type="none"><a href="../Beauty_page/Allskincare.html">Skin Care</a>
+  <li>Cleansers</li>
+  <li>Exfoliators</li>
+  <li>Eye Creams & Treatments</li>
+  <li>Face Mists & Toners</li>
+  <li>Face Moisturizers</li>
+  <li>Facial Masks</li>
+  <li>Gifts & Sets</li>
+  <li>Lip Balms & Treatments</li>
+  <li>Makeup Remover</li>
+  <li>Serums</li>
+  <li>Sunscreen</li>
+  <li>Tools & Devices</li>
+  <li>Bath & Body</li>
+  </ul>
+  </div>
+
+  <div id="menscloths">
+  <ul type="none"><a href="../Beauty_page/AllhairCare.html">Hair Care</a>
+  <li>Hair & Scalp Treatments</li>
+  <li>Hair Dryers & Styling Tools</li>
+  <li>Eye Creams & Treatments</li>
+  <li>Hair Styling Products</li>
+  <li>Shampoo & Conditioner</li>
+  </ul>
+  <br>
+  <ul type="none"><a href="../Beauty_page/Allfragrance.html">Fragrance</a>
+  <li>Candles & Diffusers</li>
+  <li>Designer Fragrance</li>
+  <li>Gifts & Sets</li>
+  <li>Perfume</li>
+  </ul>
+ 
+  <br>
+  <ul type="none"><a href="../Beauty_page/Alltools.html">Tools, Brushes & Makeup Bags</a>
+  <li>Bath & Body Tools</li>
+  <li>Hair Styling Tools</li>
+  <li>Makeup Brushes, Tools, & Bags</li>
+  <li>Skin Care Tools</li>
+  </ul>
+  <br>
+  <ul type="none"><a href="../Beauty_page/Allmensgrooming.html">Men's Grooming & Cologne</a></ul>
+  </div>
+
+`
+
+}
+
+export { beauty }
